0872f8a3f246384aed914783c4b8fe22bb745ebd
Artem Govorov <artem.govorov@gmail.com>
2017-03-21 16:43:34 +1000

    Initial commit

---
+++ quokka.js
@@ -0,0 +1,9 @@
+// The config also be placed into package.json or global quokka config,
+// see https://quokkajs.com/docs/configuration.html
+({
+  babel: true
+})
+
+import value from './myModule';
+
+console.log(value);
